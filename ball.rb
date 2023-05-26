8d7fbc2e19b4cf698fa735205b13b4b981841171
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-26 13:01:30 +1200

    actualise

--- ball.rb
+++ ball.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class Ball
-=begin 
-circled 
-A : 24B6
-B: 24B7
-C: 24B8
-D: 24B9
-E: 24BA
-F: 24BB
-
-full black circle: 23FA
-
-empty circle : 20DD
-=end
+  #
+  # circled
+  # A : 24B6
+  # B: 24B7
+  # C: 24B8
+  # D: 24B9
+  # E: 24BA
+  # F: 24BB
+  #
+  # full black circle: 23FA
+  #
+  # empty circle : 20DD
   attr_accessor :symbol, :color_name
 
   def initialize(color_name)
