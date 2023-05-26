3b76389ed737d0af3f22b4937207aba1a355795b
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-27 10:10:55 +1200

    subtract 1 from the input numbers, so the access to the array works right

--- human_player.rb
+++ human_player.rb
@@ -8,7 +8,7 @@ class HumanPlayer
   end
 
   def choose_4_colors(color_pallette, round)
-    puts "Round #{round}: Choose 4 colors (1-6) or q to quit:"
+    puts "\nRound #{round}: Choose 4 colors (1-6) or q to quit:"
     input = gets.chomp
 
     if input == 'q'
@@ -16,7 +16,7 @@ class HumanPlayer
     else
       numbers = input.split('').map(&:to_i)
       if numbers.length == 4 && numbers.all? { |element| element.between?(1, 6) }
-        color_pallette.values_at(numbers[0], numbers[1], numbers[2], numbers[3])
+        color_pallette.values_at(numbers[0]-1, numbers[1]-1, numbers[2]-1, numbers[3]-1)
       else
         puts 'You have to choose 4 numbers BETWEEN 1 and 6!:'.colorize(:red)
         choose_4_colors(color_pallette, round)
