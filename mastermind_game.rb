cb4c8059643af213441bd2ba7d6a2c0f2296a7f8
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-27 10:42:28 +1200

    add winner declaration

--- mastermind_game.rb
+++ mastermind_game.rb
@@ -13,7 +13,7 @@ class MastermindGame
     @human_player = HumanPlayer.new(mode)
     @comp_player = CompPlayer.new(mode)
     @guess_rows = []
-    @color_pallette = [Ball.new(:red), Ball.new(:blue), Ball.new(:green), Ball.new(:yellow), Ball.new(:magenta), Ball.new(:black)]
+    @color_pallette = [Ball.new(:red), Ball.new(:blue), Ball.new(:green), Ball.new(:yellow), Ball.new(:magenta), Ball.new(:light_black)]
     @game_round = 1;
   end
 
@@ -33,9 +33,10 @@ class MastermindGame
   def play_human_breaks()
     # make and safe secret code
     self.comp_player.make_secret_code(self.color_pallette)
-    
+    print_colors(self.comp_player.secret_code)
     puts "The 6 colors you can choose from (1-6):"
     print_colors(self.color_pallette)
+    puts ""
     for i in 0..11
       chosen_colors = human_player.choose_4_colors(self.color_pallette, self.game_round)
       unless chosen_colors
@@ -43,17 +44,55 @@ class MastermindGame
       end
       guess_rows.push(chosen_colors)
       print_colors(guess_rows[i])
-      comp_player.give_feedback(guess_rows[i])
+      circle_nums = comp_player.give_feedback(guess_rows[i])
+      if circle_nums.first == 4
+        declare_breaker_wins()
+      else
+        print_full_circles(circle_nums.first)
+        print_empty_circles(circle_nums.last)
+      end
     end
+    declare_creater_wins()
   end
 
   def print_colors(balls_to_print)
     balls_to_print.length.times do |i|
-      print " #{color_pallette[i].symbol} "
+      print " #{balls_to_print[i].symbol} "
     end
-    puts ""
   end
 
+  def print_empty_circles(number)
+    number.times do
+      print " \u{20DD} "
+    end
+  end
+
+  def print_full_circles(number)
+    print "   "
+    number.times do
+      print "\u{23FA} "
+    end
+  end
+
+  def declare_breaker_wins()
+    if self.mode == 2
+      puts "\nCongrats, you guessed the right code. You win!"
+      quit_game()
+    else
+      puts "\nYou loose. The computer guessed your secret code. Try again."
+      quit_game()
+    end
+  end
+
+  def declare_creater_wins()
+    if self.mode == 1
+      puts "\nCongrats, you beat the computer. Your Code was too strong!"
+      quit_game()
+    else
+      puts "\nSorry, the 12 guesses are up. Next time you got it!"
+      quit_game()
+    end
+  end
 
   def quit_game()
     return
