3e092b23d5124afc0d4e9a14de70b9a251663080
Lina <117147821+LinA636@users.noreply.github.com>
2023-05-26 13:01:15 +1200

    adjust choose_4_colors

--- human_player.rb
+++ human_player.rb
@@ -1,29 +1,31 @@
-class HumanPlayer 
+# frozen_string_literal: true
+
+class HumanPlayer
   attr_reader :mode
 
   def initialize(mode)
-    @mode = mode 
+    @mode = mode
   end
 
   def choose_4_colors(color_pallette, round)
     puts "Round #{round}: Choose 4 colors (1-6) or q to quit:"
     input = gets.chomp
 
-    if input == "q"
-      return quit_game()
+    if input == 'q'
+      quit_game
     else
-      numbers = input.split("").map {|element| element.to_i}
-      if numbers.all?{|element| element.between?(1,6)}
-        return color_pallette.values_at(numbers)
+      numbers = input.split('').map(&:to_i)
+      if numbers.length == 4 && numbers.all? { |element| element.between?(1, 6) }
+        color_pallette.values_at(numbers[0], numbers[1], numbers[2], numbers[3])
       else
-        puts "You have to choose a number BETWEEN 1 and 6!:".colorize(:red)
+        puts 'You have to choose 4 numbers BETWEEN 1 and 6!:'.colorize(:red)
         choose_4_colors(color_pallette, round)
       end
     end
   end
 
-  def quit_game()
-    puts "Was fun playing with you. See you next time!"
-    return false
+  def quit_game
+    puts 'Was fun playing with you. See you next time!'
+    false
   end
 end
